c1e662f7a9dd010aa4023e08c30374b01ecda9ea
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-10-10 20:42:09 -0400

    Add pages for other languages.
    Fixes issue #8. Added pages for java, javascript and python. Also
    modified test.rb to gen_repos.rb which now takes a command line argument
    for the language.

--- lib/gen_repos.rb
+++ lib/gen_repos.rb
@@ -1,6 +1,6 @@
 require_relative 'repository_downloader'
 
 repo_downloader = RepositoryDownloader.new
-repos = repo_downloader.download('ruby')
+repos = repo_downloader.download(ARGV[0])
 
 repos.each { |repo| puts "#{repo.name}" }

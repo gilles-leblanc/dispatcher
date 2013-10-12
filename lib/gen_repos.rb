14fbb8aa5d4a803cfe8b040908e10345412df720
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-10-11 20:48:45 -0400

    Changed repo names to link on GitHub page.
    Also added project description next to links. fixes #9

--- lib/gen_repos.rb
+++ lib/gen_repos.rb
@@ -3,4 +3,4 @@ require_relative 'repository_downloader'
 repo_downloader = RepositoryDownloader.new
 repos = repo_downloader.download(ARGV[0])
 
-repos.each { |repo| puts "#{repo.name}" }
+repos.each { |repo| puts "<li><a href='https://github.com/#{repo.full_name}'>#{repo.name}</a> #{repo.description} </li>" }

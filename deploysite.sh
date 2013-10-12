d79e9d2bc6cbdfcbc53ad2256b2fec3914c99995
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-10-11 21:58:59 -0400

    Added shell script to deploy site
    Along with first test deploy (in it's current form should commit some
    site files that were modified).

---
+++ deploysite.sh
@@ -0,0 +1,17 @@
+# Deploy the generated site to GitHub
+
+# Commit current modifications
+git add .
+git commit
+
+# Create a temp dir to copy the files while we switch branches
+TEMP_CP_DIR="../dispatcher_site_temp"
+if [ -d  "$TEMP_CP_DIR" ]; then
+  rm -r $TEMP_CP_DIR
+fi
+
+mkdir $TEMP_CP_DIR
+cp -r /site/* $TEMP_CP_DIR
+
+git checkout gh-pages
+cp -rf $TEMP_CP_DIR .
--- gensite.sh
+++ gensite.sh
@@ -1,2 +1,15 @@
+# Shell script to automate the creation of the web site
+
+# Generate the project pages for the different languages
 cd lib/
-ruby test.rb
+ruby gen_repos.rb ruby > ../site/_includes/ruby_repos.html
+ruby gen_repos.rb python > ../site/_includes/python_repos.html
+ruby gen_repos.rb javascript > ../site/_includes/javascript_repos.html
+ruby gen_repos.rb java > ../site/_includes/java_repos.html
+cd ../site
+
+# Do a jekyll build to be able to preview the site locally
+jekyll build
+cd ..
+
+echo Site creation done

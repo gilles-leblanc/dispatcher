c1403c279707a578175bfab57245d60ce0d96b8a
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-10-14 19:04:08 -0400

    Changed number of presented repos
    Changed from 25 to 30.

--- lib/gen_repos.rb
+++ lib/gen_repos.rb
@@ -3,7 +3,7 @@ require_relative 'repository_downloader'
 repo_downloader = RepositoryDownloader.new
 repos = repo_downloader.download(ARGV[0])
 
-repos.shuffle.take(25).each do |repo|
+repos.shuffle.take(30).each do |repo|
   puts "<li><a href='https://github.com/#{repo.full_name}'>#{repo.name}</a> " +
   "#{repo.description} </li>"
 end

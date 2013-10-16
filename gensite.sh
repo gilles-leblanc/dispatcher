45c2630358cfaf0391c52979c41707b43d56d73c
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-10-15 23:28:00 -0400

    Automatic GitHub page update

--- gensite.sh
+++ gensite.sh
@@ -3,9 +3,9 @@
 # Generate the project pages for the different languages
 cd lib/
 ruby gen_repos.rb ruby > ../site/_includes/ruby_repos.html
-# ruby gen_repos.rb python > ../site/_includes/python_repos.html
-# ruby gen_repos.rb javascript > ../site/_includes/javascript_repos.html
-# ruby gen_repos.rb java > ../site/_includes/java_repos.html
+ruby gen_repos.rb python > ../site/_includes/python_repos.html
+ruby gen_repos.rb javascript > ../site/_includes/javascript_repos.html
+ruby gen_repos.rb java > ../site/_includes/java_repos.html
 cd ../site
 
 # Do a jekyll build to be able to preview the site locally
